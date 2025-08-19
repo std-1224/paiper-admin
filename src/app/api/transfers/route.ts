2d4529262a02b84051fb94abf682d6ae6b43b85b
std-1224 <kalkaudy@gmail.com>
2025-08-19 23:28:24 +0700

    remove console userid

--- src/app/api/transfers/route.ts
+++ src/app/api/transfers/route.ts
@@ -12,7 +12,6 @@ export async function GET(req: NextRequest) {
         { status: 400 }
       );
     }
-    console.log("userId ------>", userId)
     // Fetch all data in parallel
     const [
       { data: receivedTransfers },
